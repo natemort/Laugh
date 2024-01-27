b433cfe9c897e8257ae13fbe68a61e2ed672b5cb
squiishyy <joe@union.ai>
2024-01-27 10:45:26 -0800

    camera works now

--- code/character/ArenaCamera.cs
+++ code/character/ArenaCamera.cs
@@ -8,9 +8,9 @@ public partial class ArenaCamera : Camera2D
 
 	[Export] public float MoveSpeed = 0.5f;
 	[Export] public float ZoomSpeed = 0.05f;
-	[Export] public float MinZoom = 1.5f;
-	[Export] public float MaxZoom = 5f;
-	public Vector2 Margin = new Vector2(400f, 400f);
+	[Export] public float MinZoom = 5f; // camera wont zoom closer than this
+	[Export] public float MaxZoom = 0.5f; // camera wont zoom farther than this
+	public Vector2 Margin = new Vector2(400f, 250f);
 
 	public HashSet<Node2D> Targets = new HashSet<Node2D>();
 	public Vector2 ScreenSize;
@@ -18,8 +18,7 @@ public partial class ArenaCamera : Camera2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
-
+		ScreenSize = this.GetViewportRect().Size;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -29,27 +28,35 @@ public partial class ArenaCamera : Camera2D
 		var positions = Vector2.Zero;
 		foreach(Node2D n in Targets) positions += n.Position;
 		positions /= Targets.Count;
-		Position = Position.Lerp(positions, MoveSpeed);
+		Position = Position.Lerp(positions, MoveSpeed * (float)delta);
+		
 		
 		// Zoom handling
 		Rect2 rect = new Rect2(Position, Vector2.One);
 		foreach (Node2D n in Targets)
 		{
-			rect.Expand(n.Position);
+			rect = rect.Expand(n.Position);
+			GD.Print("include: " + n.GlobalPosition);
 		}
 		
 		rect = rect.GrowIndividual(Margin.X, Margin.Y, Margin.X, Margin.Y);
-		float max = Math.Max(rect.Size.X, rect.Size.Y);
-		float zoom;
+		float z;
+		GD.Print("x:" + rect.Size.X + " y*aspect:" + (rect.Size.Y * ScreenSize.Aspect()));
 		if (rect.Size.X > rect.Size.Y * ScreenSize.Aspect())
 		{
-			zoom = Math.Clamp(rect.Size.X / ScreenSize.X, MinZoom, MaxZoom);
+			z = 1 / Math.Clamp(rect.Size.X / ScreenSize.X, MaxZoom, MinZoom);
+			GD.Print("x is bigger");
+			GD.Print(rect.Size.X / ScreenSize.X);
 		} else
 		{
-			zoom = Math.Clamp(rect.Size.Y / ScreenSize.Y, MinZoom, MaxZoom);
+			z = 1 / Math.Clamp(rect.Size.Y / ScreenSize.Y, MaxZoom, MinZoom);
+			GD.Print("y is bigger ");
+			GD.Print(rect.Size.Y / ScreenSize.Y);
 		}
+		// GD.Print(z);
 		
-		Zoom = Zoom.Lerp(Vector2.One * zoom, ZoomSpeed);
+		Zoom = Zoom.Lerp(Vector2.One * z, ZoomSpeed * (float)delta);
+		GD.Print(Zoom);
 
 	}
 }
--- code/character/joe_scene.cs
+++ code/character/joe_scene.cs
@@ -9,27 +9,19 @@ public partial class joe_scene : Node2D
 		this.GetNode("ArenaCamera");
 		GD.Print("camera ready");
 		ArenaCamera cam = this.GetNode<ArenaCamera>("Camera2D");
-		if (cam.Targets.Add(this.GetNode<Node2D>("player1")))
-		{
-			GD.Print("added p1");
-		}
+		cam.Targets.Add(this.GetNode<Node2D>("player1"));
+		cam.Targets.Add(this.GetNode<Node2D>("player2"));
 
-		if (cam.Targets.Add(this.GetNode<Node2D>("player2")))
-		{
-			GD.Print("added p2");
-		}
-		GD.Print("wfttfff");
-
-		foreach (var node2D in cam.Targets)
-		{
-			GD.Print(node2D.Position);
-		}
-		
-		GD.Print(cam.Targets.ToString());
+		Rect2 viewport = this.GetViewportRect();
+		// cam.LimitLeft = (int) viewport.Position.X;
+		GD.Print(cam.LimitLeft);
+		// cam.LimitRight = (int) viewport.End.X;
+		GD.Print(cam.LimitRight);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		
 	}
 }
